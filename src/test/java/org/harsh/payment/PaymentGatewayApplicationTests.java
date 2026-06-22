e419dcde334edfaffd47699bf6743b8bde08744c
Harsh Anand <harsh25061999@gmail.com>
2026-06-22 22:25:59 +0530

    initial commit

---
+++ src/test/java/org/harsh/payment/PaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.harsh.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
